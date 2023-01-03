f508398dffb634f34a1c723da5c7bebc5e28d5ab
nidhikumari <kumari9936nidhi@gmail.com>
2023-01-03 19:08:21 +0530

    objects

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -13,4 +13,18 @@ numbers.forEach((n) => n.toFixed);
 let user = [1, "Nidhi"];
 let mySize = 3;
 console.log(mySize);
+function calculateTax(income, taxYear = 2022) {
+    if (taxYear < 50000)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax(10000);
+let employee = {
+    id: 1,
+    name: "",
+    retire: (date) => {
+        console.log(date);
+    },
+};
+employee.name = "Nidhi";
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -35,3 +35,18 @@ function calculateTax(income: number, taxYear = 2022) {
 }
 
 calculateTax(10_000);
+
+// Objects
+
+let employee: {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: "",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
+employee.name = "Nidhi";
