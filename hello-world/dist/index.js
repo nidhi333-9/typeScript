372ed84f199671ca758d19f48fc45e9bee112c28
nidhikumari <kumari9936nidhi@gmail.com>
2023-01-04 18:58:40 +0530

    type aliases

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,24 +1,4 @@
 "use strict";
-let sales = 123456789;
-let course = "Typescript";
-let is_published = true;
-let level;
-level = 1;
-level = "a";
-function render(document) {
-    console.log(document);
-}
-let numbers = [1, 3, 4];
-numbers.forEach((n) => n.toFixed);
-let user = [1, "Nidhi"];
-let mySize = 3;
-console.log(mySize);
-function calculateTax(income, taxYear = 2022) {
-    if (taxYear < 50000)
-        return income * 1.2;
-    return income * 1.3;
-}
-calculateTax(10000);
 let employee = {
     id: 1,
     name: "",
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,48 +1,49 @@
-let sales: number = 123_456_789;
-let course: string = "Typescript";
-let is_published: boolean = true;
+// let sales: number = 123_456_789;
+// let course: string = "Typescript";
+// let is_published: boolean = true;
 
-let level;
-level = 1;
-level = "a";
+// let level;
+// level = 1;
+// level = "a";
 
-function render(document: number) {
-  console.log(document);
-}
+// function render(document: number) {
+//   console.log(document);
+// }
 
-let numbers: number[] = [1, 3, 4];
-numbers.forEach((n) => n.toFixed);
+// let numbers: number[] = [1, 3, 4];
+// numbers.forEach((n) => n.toFixed);
 
 // Tuples
 
-let user: [number, string] = [1, "Nidhi"];
+// let user: [number, string] = [1, "Nidhi"];
 
 // Enumes
 
-const enum Size {
-  Small = 1,
-  Medium,
-  Large,
-}
+// const enum Size {
+//   Small = 1,
+//   Medium,
+//   Large,
+// }
 
-let mySize: Size = Size.Large;
-console.log(mySize);
+// let mySize: Size = Size.Large;
+// console.log(mySize);
 // functions
 
-function calculateTax(income: number, taxYear = 2022) {
-  if (taxYear < 50_000) return income * 1.2;
-  return income * 1.3;
-}
+// function calculateTax(income: number, taxYear = 2022) {
+//   if (taxYear < 50_000) return income * 1.2;
+//   return income * 1.3;
+// }
 
-calculateTax(10_000);
+// calculateTax(10_000);
 
 // Objects
 
-let employee: {
+type Employee = {
   readonly id: number;
   name: string;
   retire: (date: Date) => void;
-} = {
+};
+let employee: Employee = {
   id: 1,
   name: "",
   retire: (date: Date) => {
@@ -50,3 +51,5 @@ let employee: {
   },
 };
 employee.name = "Nidhi";
+
+//
