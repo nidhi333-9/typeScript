b7b25addc419412e1b6153050f718d88b0c4501d
nidhikumari <kumari9936nidhi@gmail.com>
2023-01-15 16:35:18 +0530

    small changes

--- type/index.js
+++ type/index.js
@@ -4,7 +4,7 @@ const button = document.querySelector("button");
 
 function add(num1, num2) {
   if (typeof num1 === "number" && typeof num2 === "number") {
-    return num1 + num2;
+    return +num1 + +num2;
   } else {
     return +num1 + +num2;
   }
